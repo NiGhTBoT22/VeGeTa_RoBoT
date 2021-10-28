afe2a73edbfcfb65708a67a311d30d7b130c481c
NiGhT_BoT_22 <88623043+NiGhTBoT22@users.noreply.github.com>
2021-10-28 05:37:38 +0530

    Update welcome.py

--- KURUMIBOT/modules/welcome.py
+++ KURUMIBOT/modules/welcome.py
@@ -177,7 +177,7 @@ def new_member(update: Update, context: CallbackContext):
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
                 update.effective_message.reply_text(
-                    "𝓞𝓱, 𝓖𝓮𝓷𝓸𝓼 ? 𝓣𝓱𝓮 𝓢𝓾𝓹𝓮𝓻 𝓢𝓪𝓲𝔂𝓪𝓷 𝓟𝓻𝓲𝓷𝓬𝓮 𝓲𝓽𝓼𝓮𝓵𝓯 𝓳𝓾𝓼𝓽 𝓳𝓸𝓲𝓷𝓮𝓭🔹.", 
+                    "𝓞𝓱, 𝓖𝓮𝓷𝓸𝓼 ? 𝓣𝓱𝓮 𝓢𝓾𝓹𝓮𝓻 𝓢𝓪𝓲𝔂𝓪𝓷 𝓟𝓻𝓲𝓷𝓬𝓮 𝓲𝓽𝓼𝓮𝓵𝓯 𝓳𝓾𝓼𝓽 𝓳𝓸𝓲𝓷𝓮𝓭🔹." https://telegra.ph/file/60606d9b1b0ff21d42e75.jpg , 
                     reply_to_message_id=reply) 
                 welcome_log = (f"{html.escape(chat.title)}\n"
                                f"#USER_JOINED\n"
